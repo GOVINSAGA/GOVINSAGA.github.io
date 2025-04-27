0be66b29e8d29f53a031606c797492f83fdbcbf4
GOVINSAGA <73192061+GOVINSAGA@users.noreply.github.com>
2025-04-27 12:09:54 +0530

    27 april

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-        anchor.addEventListener("click", function (e) {
-          e.preventDefault();
-          const targetId = this.getAttribute("href");
-          const targetElement = document.querySelector(targetId);
-          if (targetElement) {
-            // Calculate position considering sticky header height
-            const headerOffset =
-              document.querySelector("header").offsetHeight + 20; // Get header height + extra space
-            const elementPosition = targetElement.getBoundingClientRect().top;
-            const offsetPosition =
-              elementPosition + window.pageYOffset - headerOffset;
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      // Calculate position considering sticky header height
+      const headerOffset =
+        document.querySelector("header").offsetHeight + 20; // Get header height + extra space
+      const elementPosition = targetElement.getBoundingClientRect().top;
+      const offsetPosition =
+        elementPosition + window.pageYOffset - headerOffset;
 
-            window.scrollTo({
-              top: offsetPosition,
-              behavior: "smooth",
-            });
-          }
-        });
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
       });
+    }
+  });
+});
